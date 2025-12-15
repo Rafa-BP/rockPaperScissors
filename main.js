775985f3e46ed0fe6bf8e2fb9c0611a9db71f1f7
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2025-12-15 19:48:00 -0300

    Finished game logic, now game stops when any score reaches 5.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
-let rounds = 0;
+let debounce = 0;
 
 function getComputerChoice() {
     let ran = Math.floor(Math.random() * 3) + 1;
@@ -14,14 +14,27 @@ function getComputerChoice() {
 };
 
 function displayResults(result) {
+
     const resultText = document.getElementById("result");
-    resultText.textContent = result;
+        resultText.textContent = result;
+
+        const humanResult = document.getElementById("human-score");
+        humanResult.textContent = `You: ${humanScore}`;
+
+        const computerResult = document.getElementById("computer-score");
+        computerResult.textContent = `Computer: ${computerScore}`;
+
     if (result === "won") {
         resultText.style.color = "greenyellow";
     } else if (result === "lost") {
         resultText.style.color = "red";
     } else {
         resultText.style.color = "gray";
+    };
+
+    if (humanScore == 5 || computerScore == 5) {
+        resultText.textContent = `The Winner is: ${humanScore == 5 ? "You" : "Computer"}`;
+        debounce = 1;
     };
 };
 
@@ -30,38 +43,46 @@ function playRound(human, computer) {
         return displayResults("draw");
     } else if (human == "rock") {
         if (computer == "scissors") {
+            humanScore++;
             return displayResults("won");
         } else {
+            computerScore++;
             return displayResults("lost");
         };
     } else if (human == "scissors") {
-        if (computer = "paper") {
+        if (computer == "paper") {
+            humanScore++;
             return displayResults("won");
         } else {
+            computerScore++;
             return displayResults("lost");
         };
     } else {
         if (computer == "rock") {
+            humanScore++;
             return displayResults("won");
         } else {
+            computerScore++;
             return displayResults("lost");
         };
     };
 };
 
 document.addEventListener('click', e => {
-    switch (e.target.id) {
-        case "rock": {
-            playRound("rock", getComputerChoice());
-            break;
-        };
-        case "paper": {
-            playRound("paper", getComputerChoice());
-            break;
-        };
-        case "scissors": {
-            playRound("scissors", getComputerChoice());
-            break;
+    if (debounce == 0) {
+        switch (e.target.id) {
+            case "rock": {
+                playRound("rock", getComputerChoice());
+                break;
+            };
+            case "paper": {
+                playRound("paper", getComputerChoice());
+                break;
+            };
+            case "scissors": {
+                playRound("scissors", getComputerChoice());
+                break;
+            };
         };
     };
 });
